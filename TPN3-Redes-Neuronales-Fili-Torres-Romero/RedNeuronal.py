45e785b6df28cf649e65f8613f1365d350fe7733
gonzalo <gonzaromero2007@gmail.com>
2018-05-12 16:11:43 -0300

    Gloriosa red neuronal!

--- TPN3-Redes-Neuronales-Fili-Torres-Romero/RedNeuronal.py
+++ TPN3-Redes-Neuronales-Fili-Torres-Romero/RedNeuronal.py
@@ -8,11 +8,11 @@ class Neurona:
         self.funcionActivacion = funcionActivacion
         for i in range(numeroPesos):
             self.pesos.append((random.randrange(-1000,1000)/1000.0))
-        print("Pesos")
-        print(self.pesos)
+        #print("Pesos")
+        #print(self.pesos)
         self.sesgo = random.randrange(-1000,1000)/1000.0
-        print("Sesgos")
-        print(self.sesgo)
+        #print("Sesgos")
+        #print(self.sesgo)
 
     def actualizarNeurona(self):
         pass
@@ -40,9 +40,6 @@ class Neurona:
         return self.salida
 
 
-    def funcionActivacion(self):
-        pass
-
 
 class Capa:
 
@@ -56,24 +53,57 @@ class Capa:
             self.neuronas.append(neurona)
 
     def actualizarCapaFinal(self, salidaDeseada, ritmoAprendizaje):
+        self.pesosAnteriores = []
+        self.deltas = []
         for i in range(len(self.neuronas)):
             if self.funcionActivacion == "sigmoidal":
                 delta = (salidaDeseada[i] - self.salidas[i]) * (1 - self.salidas[i])
             if self.funcionActivacion == "lineal":
                 pass
-            variacionSesgo = ritmoAprendizaje * delta * self.neuronas[i].sesgo
+            self.deltas.append(delta)
+            variacionSesgo = ritmoAprendizaje * delta * -1
             variacionPesos = []
             for j in range(len(self.entradaCapa)):
                 variacionPesos.append(ritmoAprendizaje * delta * self.entradaCapa[j])
 
             self.neuronas[i].sesgo = self.neuronas[i].sesgo + variacionSesgo
             for j in range(len(self.entradaCapa)):
+                self.pesosAnteriores.append(self.neuronas[i].pesos[j])
                 self.neuronas[i].pesos[j] = self.neuronas[i].pesos[j] + variacionPesos[j]
-            print("Nuevos Parametros")
-            print(self.neuronas[i].pesos)
-            print(self.neuronas[i].sesgo)
-            print ("------------------------")
+            #print("Nuevos Parametros")
+            #print(self.neuronas[i].pesos)
+            #print(self.neuronas[i].sesgo)
+            #print ("------------------------")
 
+    def actualizarCapaOculta(self, deltasCA, pesosCA, ritmoAprendizaje ):
+        self.pesosAnteriores = []
+        self.deltas = []
+        aux = 0
+        for i in range(len(self.neuronas)):
+            for j in range(len(deltasCA)):
+                aux = aux + deltasCA[j] * pesosCA[j]
+
+            if self.funcionActivacion == "sigmoidal":
+                aux = aux * (1 - self.salidas[i])
+
+            if self.funcionActivacion == "lineal":
+                pass
+            self.deltas.append(aux)
+
+            variacionSesgo = ritmoAprendizaje * self.deltas[i] * -1
+            variacionPesos = []
+            for j in range(len(self.entradaCapa)):
+                variacionPesos.append(ritmoAprendizaje * self.deltas[i] * self.entradaCapa[j])
+
+            self.neuronas[i].sesgo = self.neuronas[i].sesgo + variacionSesgo
+            for j in range(len(self.entradaCapa)):
+                self.pesosAnteriores.append(self.neuronas[i].pesos[j])
+                self.neuronas[i].pesos[j] = self.neuronas[i].pesos[j] + variacionPesos[j]
+            #print("Nuevos Parametros")
+            #print(self.neuronas[i].pesos)
+            #print(self.neuronas[i].sesgo)
+            #print ("------------------------")
+            aux = 0
 
 
 
@@ -104,7 +134,7 @@ class RedNeuronal:
                 self.capas.append(capa)
         capa = Capa(tamanoSalida, tamanoCapaOculta[-1], funcionActivacion[-1])
         self.capas.append(capa)
-        #print (len(self.capas))
+        ##print (len(self.capas))
 
     def entrenar(self, data, salidaDeseada, epocas):
         for k in range(epocas):
@@ -114,13 +144,22 @@ class RedNeuronal:
                         salidaCapaAnterior = self.capas[i].calcularSalida(data[j])
                     else:
                         salidaCapaAnterior = self.capas[i].calcularSalida(salidaCapaAnterior)
-                    print(salidaCapaAnterior)
+                    #print(salidaCapaAnterior)
                 for i in range(len(self.capas)):
                     if i == 0:
                         self.capas[-i - 1].actualizarCapaFinal(salidaDeseada[j], self.ritmoAprendizaje)
                     else:
-                        pass
-                    #self.capas[-i - 1].actualizarCapa(salidaDeseada)
+                        self.capas[-i - 1].actualizarCapaOculta(self.capas[-i].deltas, self.capas[-i].pesosAnteriores, self.ritmoAprendizaje)
+
+    def clasificar(self, vector):
+        pass
+
+    def buscarSalidas(self, vector):
+        for i in range (len(self.capas)):
+            vector = self.capas[i].calcularSalida(vector)
+        return vector
+
+
 
 
 
@@ -135,14 +174,11 @@ def main():
     ent = [0.80, 0.35, 0.43]
     data = []
     data.append(ent)
-    # data.append(ent)
-    # data.append(ent)
-    # data.append(ent)
-    print (data)
+    #print (data)
 
     salidaDeseada = [[-1]]#]
 
-    epocas = 1
+    epocas = 10
     numeroCapasOcultas = 1
     tamanoEntrada = 3
     tamanoCapaOculta = [2]
@@ -152,7 +188,10 @@ def main():
     Red = RedNeuronal(tamanoEntrada, tamanoCapaOculta, tamanoSalida, numeroCapasOcultas, funcionActivacion, ritmoAprendizaje)
     #data = leerDataSet(/path/to/dataset)
     Red.entrenar(data, salidaDeseada, epocas)
-    #Red.claficar(vector)
+    categoria = Red.buscarSalidas(ent)
+    print (categoria)
+
+
 
 
 
